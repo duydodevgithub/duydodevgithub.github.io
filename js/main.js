17f3d4d26c540fafda658a8c2b7118d667118a8f
DuyDo <duydo.dev@gmail.com>
2018-04-17 15:37:37 -0500

    update icon8 to social icon

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@
 var d = new Date();
 d = d.getHours();
 if(d > 18) {
-    $("#sayHello").html("Good Evening! Welcome to my page");
+    $("#sayHello").html("Good Evening!" + "<div>Welcome to my page</div>");
 } else if (d > 12) {
-    $("#sayHello").html("Good Afternoon! Welcome to my page");
+    $("#sayHello").html("Good Afternoon!" + "<div>Welcome to my page</div>");
 } else if (d > 0) {
-    $("#sayHello").html("Good Morning! Welcome to my page");
+    $("#sayHello").html("Good Morning!" + "<div>Welcome to my page</div>");
 } else {
     $("#sayHello").html("Welcome to my page!");    
 }
